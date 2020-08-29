e32ff1f7668781171ced09a5bb9f6800f6b7d0ff
aciadmin <soumukhe@cisco.com>
2020-08-28 23:14:06 -0500

    changed mso_pass.sh with a more secure pass generator

--- mso_pass.sh
+++ mso_pass.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "hello\!" | base64 
+echo ""
+echo A simple password:
+echo "hello\!" | base64
+echo ""
+echo A more secure password is:
+echo head -c 21 /dev/urandom | base64
+echo ""
